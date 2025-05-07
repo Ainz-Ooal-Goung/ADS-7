57f51c712d248eebe8166c4ceca43aae8d252c67
Ainz-Ooal-Goung <vadimdfl129@gmail.com>
2025-05-07 21:05:24 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -2,35 +2,35 @@
 #include "train.h"
 
 Train::Train()
-	: countOp(0), first(nullptr) {}
+  : countOp(0), first(nullptr) {}
 
 void Train::addCar(bool light) {
-	Car *node = new Car{light, nullptr, nullptr};
-	if (!first) {
-		node->next = node;
-		node->prev = node;
-		first = node;
-	} else {
-		Car *last = first->prev;
-		last->next = node;
-		node->prev = last;
-		node->next = first;
-		first->prev = node;
-	}
+  Car *node = new Car{light, nullptr, nullptr};
+  if (!first) {
+    node->next = node;
+    node->prev = node;
+    first = node;
+  } else {
+    Car *last = first->prev;
+    last->next = node;
+    node->prev = last;
+    node->next = first;
+    first->prev = node;
+  }
 }
 
 int Train::getLength() {
-	if (!first) return 0;
-	int length = 0;
-	Car *cur = first;
-	do {
-		cur = cur->next;
-		++length;
-		++countOp;
-	} while (cur != first);
-	return length;
+  if (!first) return 0;
+  int length = 0;
+  Car *cur = first;
+  do {
+    cur = cur->next;
+    ++length;
+    ++countOp;
+  } while (cur != first);
+  return length;
 }
 
 int Train::getOpCount() {
-	return countOp;
+  return countOp;
 }
